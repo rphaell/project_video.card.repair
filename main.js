7a2a2347f9f1d6aee24b28f7efed7dbc916be5cb
Raphael <phael92br@gmail.com>
2024-04-05 09:45:54 -0300

    fazendo updt

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // change navbar style on scroll
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
-})
+// window.addEventListener('scroll', () => {
+//     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+// })
 
 // show/hide faq answer
 
